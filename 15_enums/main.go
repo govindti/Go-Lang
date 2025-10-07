62c70910d4b5dd7cd5b1e03deeccdc6a6ba948fd
govindti <108tiwari.g@gmail.com>
2025-10-07 22:00:29 +0530

    Add implementation of OrderStatus enum and changeOrderStatus function

---
+++ 15_enums/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type OrderStatus int
+
+// iota if int
+const (
+	Received OrderStatus = iota
+	Confirmed
+	Prepared
+	Delivered
+)
+
+//  if string
+// const (
+// 	Received OrderStatus = "received"
+// 	Confirmed ="confirmed"
+// 	Prepared ="prepaerd"
+// 	Delivered ="delivered"
+// )
+
+func changeOrderStatus(status OrderStatus) {
+	fmt.Println("Changing Order Status to", status)
+}
+
+func main() {
+	changeOrderStatus(Prepared)
+}
