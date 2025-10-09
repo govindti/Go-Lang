9bd0f0f0fb3e1293102e837955fa751af3f6601c
govindti <108tiwari.g@gmail.com>
2025-10-09 22:35:53 +0530

    Refactor main.go to implement a generic stack type and demonstrate its usage with a string slice

--- 16_generics/main.go
+++ 16_generics/main.go
@@ -1,5 +1,38 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// // func printSlice[T any](items []T) {
+// // func printSlice[T interface{}](items []T) {
+// // func printSlice[T comparable{}](items []T) {
+// func printSlice[T bool | string](items []T) {
+// 	for _, item := range items {
+// 		fmt.Println(item)
+// 	}
+// }
+
+// func main() {
+// 	// can't pass string or any other dt
+// 	// printSlice([]string{"go", "js", "ts"})
+
+// 	printSlice([]string{"go", "js", "ts"})
+// 	printSlice([]bool{true, false, true})
+// }
+
 package main
 
+import "fmt"
+
+// can't use string bool in that stack
+type stack[T any] struct {
+	elements []T
+}
+
 func main() {
-	
+	myStack := stack[string]{
+		elements: []string{"go", "ts", "js"},
+	}
+	fmt.Println(myStack)
 }
